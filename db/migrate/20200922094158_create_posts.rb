ec72340439238723d364c78bfdc03850ed4d5a36
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-09-22 21:54:37 +0900

    追加

--- db/migrate/20200922094158_create_posts.rb
+++ db/migrate/20200922094158_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :nickname
+      t.text :image
+      t.string :subject
+      t.text :text
       t.timestamps
     end
   end
