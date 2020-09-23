7a7741b25c73ee8aa973c49c0c48c0e6f1152b6b
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-09-23 23:28:03 +0900

    innitial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts,only: [:index]
+  resources :posts,only: [:index,:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
