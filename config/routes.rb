5c3577f2cde94b7fd98897abf2aa2ae13369b6f1
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-10-01 21:33:11 +0900

    linkの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts,only: [:index,:new,:create]
+  resources :posts,only: [:index,:new,:create,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
