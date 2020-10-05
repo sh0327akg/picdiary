cc9c640adc08a59c9a7b84a66d34ee5414babbef
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-10-05 20:50:55 +0900

    deivseのview設定

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_04_040016) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
