4e426edf21cd6d8e8ab23a107261194bb571f3df
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-10-11 21:53:39 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:sh0327akg/picdiary.git"
 set :linked_dirs, fetch(:linked_dirs,[]).push('log','tmp/pids','tmp/cache','tmp/sockets','vendor/bundle','public/system','public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby,'2.5.1'
-set :ssh_options, auth_methods: ['publickey'],keys:['~/.ssh/key_pem.pem']
+set :ssh_options, auth_methods: ['publickey'],keys:['~/.ssh/picdiary.pem']
 set :unicorn_pid, -> {"#{shared_path}/tmp/pids/unicorn.pid"}
 set :unicorn_config_path, -> {"#{current_path}/config/unicorn.rb"}
 set :keep_releases, 5
